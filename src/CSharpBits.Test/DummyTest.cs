d45742dad6ffc06d4a19e4de150284e5fe00ca4d
Arialdo Martini <arialdomartini@gmail.com>
2019-05-16 07:50:31 +0200

    A passing, dummy test

---
+++ src/CSharpBits.Test/DummyTest.cs
@@ -0,0 +1,14 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace CSharpBits.Test
+{
+    public class DummyTest
+    {
+        [Fact]
+        void dummy_test()
+        {
+            "friends".Should().Be("friends");
+        }
+    }
+}
