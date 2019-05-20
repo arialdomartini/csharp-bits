3bea054da59611881d7a9baac398ab9d07c34899
Arialdo Martini <arialdomartini@gmail.com>
2019-05-20 09:59:53 +0200

    Playing with permutations

--- src/CSharpBits.Test/PermutationsTest.cs
+++ src/CSharpBits.Test/PermutationsTest.cs
@@ -1,216 +1,21 @@
 using System;
-using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using FluentAssertions;
 using Xunit;
-using Xunit.Abstractions;
 
 namespace CSharpBits.Test
 {
     internal static class SequenceExtensions
     {
         internal static List<char> ToSequence(this string s) => s.ToList();
+        internal static string Head(this List<string> list) => list.FirstOrDefault();
+
+        internal static List<string> RemoveItem(this List<string> list, string item) =>
+            list.Where((e, i) => i != list.IndexOf(item)).ToList();
+        internal static List<string> Tail(this List<string> list) => list.Skip(1).ToList();
+        internal static string ToSentence(this List<string> s) => s.Aggregate("", (a, i) => a + i);
         internal static string ToSentence(this List<char> sequence) => string.Join("", sequence);
-    }
-
-    public class PermutationsTest
-    {
-        private readonly ITestOutputHelper _outputHelper;
-        private readonly List<string> _empty = new List<string>();
-
-        [Fact]
-        void empty_string()
-        {
-            var result = Permutations("");
-
-            result.Should().BeEquivalentTo(_empty);
-        }
-
-        [Fact]
-        void single_char()
-        {
-            var result = Permutations("a");
-
-            result.Should().BeEquivalentTo(new List<string> {"a"});
-        }
-
-        public PermutationsTest(ITestOutputHelper outputHelper)
-        {
-            _outputHelper = outputHelper;
-        }
-
-        [Fact]
-        void multiple_chars()
-        {
-            var result = Permutations("ab").ToList();
-
-            foreach (var r in result)
-            {
-                _outputHelper.WriteLine(r);
-            }
-
-            result.OrderBy(i => i).Should().BeEquivalentTo(new List<string>
-            {
-                "ab",
-                "ba"
-            }.OrderBy(i => i));
-        }
-        
-        [Fact]
-        void generate_rests()
-        {
-            var result = Explode("abcde").ToList();
-
-            result.Should().BeEquivalentTo(new List<(string, string)>
-            {
-                ("a", "bcde"),
-                ("b", "acde"),
-                ("c", "abde"),
-                ("d", "abce"),
-                ("e", "abcd")
-            });
-        }
-
-        [Fact]
-        void augment_permutations()
-        {
-            var result = Augment(new List<string>{"x", "y"}, "abcde").ToList();
-            result.Should().BeEquivalentTo(new List<(string, string)>
-            {
-                ("xa", "bcde"),
-                ("ya", "bcde"),
-                ("xb", "acde"),
-                ("yb", "acde"),
-                ("xc", "abde"),
-                ("yc", "abde"),
-                ("xd", "abce"),
-                ("yd", "abce"),
-                ("xe", "abcd"),
-                ("ye", "abcd")
-            });
-        }
-
-        [Fact]
-        void calculate_permutations()
-        {
-            var result = Permute("abc").ToList();
-            result.Should().BeEquivalentTo(new List<string>
-            {
-                "abc",
-                "acb",
-                "bac",
-                "bca",
-                "cab",
-                "cba"
-            });
-        }
-
-        private List<string> Permute(string s)
-        {
-            return PermuteRecursive(s, new List<string>());
-        }
-
-        private List<string> PermuteRecursive(string s, List<string> existingPermutations)
-        {
-            var additions = GetCharsFrom(s);
-
-            foreach (var addition in additions)
-            {
-                //PermuteRecursive(s);
-            }
-
-            return existingPermutations;
-        }
-
-        private List<string> PermuteRecursive(string s, string existingPermutation)
-        {
-            if (string.IsNullOrEmpty(s))
-                return new List<string>();
-
-            var additions = GetCharsFrom(s);
-
-            var permutations = new List<string>();
-            foreach (var addition in additions)
-            {
-                permutations.AddRange(PermuteRecursive(CalculateRest(s, addition), existingPermutation + addition));
-            }
-
-            return permutations;
-        }
-
-        private List<string> AddTo(string addition, List<string> existingPermutations)
-        {
-            return existingPermutations.Select(p => AddToPermutation(p, addition)).ToList();
-        }
-
-        private static string AddToPermutation(string permutation, string addition) =>
-            permutation + addition;
-
-        private string CalculateRest(string s, string addition)
-        {
-            return s.RemoveChar(addition);
-        }
-
-
-        private List<string> GetCharsFrom(string s) =>
-            s.Select(x => x.ToString()).ToList();
-
-        private List<(string, string)> Augment(List<string> permutations, string s)
-        {
-            List<(string c, string rests)> explosions = Explode(s).ToList();
-
-
-            var newPerms = new List<(string, string)>();
-            foreach (var permutation in permutations)
-            {
-                foreach (var (c, rests) in explosions)
-                {
-                    newPerms.Add((permutation + c, rests));
-                }
-            }
-
-            return newPerms;
-        }
-
-        [Theory]
-        [InlineData("012345", 0, "12345")]
-        [InlineData("012345", 1, "02345")]
-        [InlineData("012345", 2, "01345")]
-        [InlineData("012345", 5, "01234")]
-        void remove_char_at_index(string s, int index, string expected)
-        {
-            s.RemoveCharAt(index).Should().Be(expected);
-        }
-
-        [Theory]
-        [InlineData("012345", "0", "12345")]
-        [InlineData("000000", "0", "0000")]
-        [InlineData("12321", "1", "2321")]
-        void remove_char(string s, string c, string expected)
-        {
-            s.RemoveChar(c).Should().Be(expected);
-        }
-
-
-        private IEnumerable<(string, string)> Explode(string s)
-        {
-            return s.Select((c, i) =>
-            {
-                return (c.ToString(), s.RemoveCharAt(i));
-            });
-        }
-
-
-
-        private List<string> Permutations(string ab)
-        {
-            throw new System.NotImplementedException();
-        }
-    }
-
-    internal static class StringExtensions
-    {
         internal static string RemoveCharAt(this string s, int index) =>
             s.Substring(0, index) + s.Substring(index+1, s.Length-index-1);
 
@@ -219,5 +24,91 @@ namespace CSharpBits.Test
             var indexOf = s.IndexOf(c, StringComparison.CurrentCulture);
             return s.Substring(0, indexOf) + s.Substring(indexOf + 1, s.Length - indexOf - 1);
         }
+    }
+
+    public class PermutationsTest
+    {
+        [Fact]
+        void remove_item()
+        {
+            var list = new List<string>{"a", "b", "c"};
+            list.RemoveItem("b").Should().BeEquivalentTo(new List<string> {"a", "c"});
+        }
+
+        [Fact]
+        void to_sentence()
+        {
+            var list = new List<string>{"a", "b", "c"};
+
+            list.ToSentence().Should().Be("abc");
+        }
+
+        [Fact]
+        void explode_from_empty()
+        {
+            var result = Explode(new List<string>(), "abc");
+
+            result.Should().BeEquivalentTo(new List<string> {"a", "b", "c"});
+        }
+
+        [Fact]
+        void explode_from_one()
+        {
+            var result = Explode(new List<string>{"a"}, "bc");
+
+            result.Should().BeEquivalentTo(new List<string> {"ab", "ac"});
+        }
+
+        [Fact]
+        void explode_from_two()
+        {
+            var result = Explode(new List<string>{"x", "y"}, "bc");
+
+            result.Should().BeEquivalentTo(new List<string> {"xb", "xc", "yb", "yc"});
+        }
+
+        [Fact]
+        void explode_rest_from_two()
+        {
+            var result = ExplodeRest(new List<string>{"x", "y"}, "bc");
+
+            result.Should().BeEquivalentTo(new List<(string, string)>
+            {
+                ("xb", "c"),
+                ("xc", "b"),
+                ("yb", "c"),
+                ("yc", "b")
+            });
+        }
+
+        private IEnumerable<string> Explode(List<string> list, string s)
+        {
+            if(!list.Any())
+                return s.Select(c => c.ToString());
+
+
+            return list.Aggregate(
+                new List<string>(),
+                (e, c) => e.Union(
+                    Extend(c, s)).ToList());
+        }
+
+        private IEnumerable<(string, string)> ExplodeRest(List<string> list, string s)
+        {
+            if(!list.Any())
+                return s.Select(c => (c.ToString(), ""));
+
+
+            return list.Aggregate(
+                new List<(string, string)>(),
+                (e, c) => e.Union(
+                    ExtendRest(c, s)).ToList());
+        }
+
+        private static IEnumerable<string> Extend(string element, string s) =>
+            s.Select(c => element + c);
+
+        private static IEnumerable<(string, string)> ExtendRest(string element, string s) =>
+            s.Select(c => (element + c, s.RemoveChar(c.ToString())));
     }
 }
