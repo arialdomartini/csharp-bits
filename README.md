82889b6436c278609123ee123710a666da126a9e
Arialdo Martini <arialdomartini@gmail.com>
2019-05-20 10:48:06 +0200

    Recursive permutation

--- README.md
+++ README.md
@@ -17,3 +17,9 @@ Leading and training `0`s should be ignored, as they are not surrounded both sid
 C# has no tail recursion optimization. Write a function that takes the elements (lambdas) of a generic recursive algorithm and unroll it so it is executed iteratively.
 
 [RecursionUnroll.cs](https://github.com/arialdomartini/csharp-bits/blob/master/src/CSharpBits.Test/RecursionUnroll.cs)
+
+
+## Permutations
+Calculate all the possible permutations of a string, with a recursive function
+
+[PermutationsTest.cs](https://github.com/arialdomartini/csharp-bits/blob/master/src/CSharpBits.Test/PermutationsTest.cs)
--- src/CSharpBits.Test/PermutationsTest.cs
+++ src/CSharpBits.Test/PermutationsTest.cs
@@ -8,17 +8,6 @@ namespace CSharpBits.Test
 {
     internal static class SequenceExtensions
     {
-        internal static List<char> ToSequence(this string s) => s.ToList();
-        internal static string Head(this List<string> list) => list.FirstOrDefault();
-
-        internal static List<string> RemoveItem(this List<string> list, string item) =>
-            list.Where((e, i) => i != list.IndexOf(item)).ToList();
-        internal static List<string> Tail(this List<string> list) => list.Skip(1).ToList();
-        internal static string ToSentence(this List<string> s) => s.Aggregate("", (a, i) => a + i);
-        internal static string ToSentence(this List<char> sequence) => string.Join("", sequence);
-        internal static string RemoveCharAt(this string s, int index) =>
-            s.Substring(0, index) + s.Substring(index+1, s.Length-index-1);
-
         internal static string RemoveChar(this string s, string c)
         {
             var indexOf = s.IndexOf(c, StringComparison.CurrentCulture);
@@ -26,89 +15,37 @@ namespace CSharpBits.Test
         }
     }
 
-    public class PermutationsTest
+    public class PermutationTest
     {
-        [Fact]
-        void remove_item()
+        private static IEnumerable<string> Permutations(string s)
+            => Permutations("", s).Select(e => e.Item1);
+
+        private static IEnumerable<(string, string)> Permutations(string acc, string rest)
         {
-            var list = new List<string>{"a", "b", "c"};
-            list.RemoveItem("b").Should().BeEquivalentTo(new List<string> {"a", "c"});
+            if (string.IsNullOrEmpty(rest))
+                return new[] {(acc, rest)};
+
+            return rest.SelectMany(c =>
+                Permutations(
+                    acc + c.ToString(),
+                    rest.RemoveChar(c.ToString())
+                ));
         }
 
         [Fact]
-        void to_sentence()
+        void as_a_recursive_function()
         {
-            var list = new List<string>{"a", "b", "c"};
+            var result = Permutations("abc");
 
-            list.ToSentence().Should().Be("abc");
-        }
-
-        [Fact]
-        void explode_from_empty()
-        {
-            var result = Explode(new List<string>(), "abc");
-
-            result.Should().BeEquivalentTo(new List<string> {"a", "b", "c"});
-        }
-
-        [Fact]
-        void explode_from_one()
-        {
-            var result = Explode(new List<string>{"a"}, "bc");
-
-            result.Should().BeEquivalentTo(new List<string> {"ab", "ac"});
-        }
-
-        [Fact]
-        void explode_from_two()
-        {
-            var result = Explode(new List<string>{"x", "y"}, "bc");
-
-            result.Should().BeEquivalentTo(new List<string> {"xb", "xc", "yb", "yc"});
-        }
-
-        [Fact]
-        void explode_rest_from_two()
-        {
-            var result = ExplodeRest(new List<string>{"x", "y"}, "bc");
-
-            result.Should().BeEquivalentTo(new List<(string, string)>
+            result.Should().BeEquivalentTo(new List<string>
             {
-                ("xb", "c"),
-                ("xc", "b"),
-                ("yb", "c"),
-                ("yc", "b")
+                "abc",
+                "acb",
+                "bac",
+                "bca",
+                "cab",
+                "cba"
             });
         }
-
-        private IEnumerable<string> Explode(List<string> list, string s)
-        {
-            if(!list.Any())
-                return s.Select(c => c.ToString());
-
-
-            return list.Aggregate(
-                new List<string>(),
-                (e, c) => e.Union(
-                    Extend(c, s)).ToList());
-        }
-
-        private IEnumerable<(string, string)> ExplodeRest(List<string> list, string s)
-        {
-            if(!list.Any())
-                return s.Select(c => (c.ToString(), ""));
-
-
-            return list.Aggregate(
-                new List<(string, string)>(),
-                (e, c) => e.Union(
-                    ExtendRest(c, s)).ToList());
-        }
-
-        private static IEnumerable<string> Extend(string element, string s) =>
-            s.Select(c => element + c);
-
-        private static IEnumerable<(string, string)> ExtendRest(string element, string s) =>
-            s.Select(c => (element + c, s.RemoveChar(c.ToString())));
     }
 }
