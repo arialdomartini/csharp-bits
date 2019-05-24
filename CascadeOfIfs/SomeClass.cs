3f8b57f05bbf4f2cf4c7517f0650528ce6605490
Arialdo Martini <arialdomartini@gmail.com>
2019-05-24 04:45:22 +0200

    An acceptance test

--- CascadeOfIfs/SomeClass.cs
+++ CascadeOfIfs/SomeClass.cs
@@ -1,16 +1,39 @@
-﻿namespace CascadeOfIfs
+﻿using FluentAssertions;
+using Xunit;
+
+namespace CascadeOfIfs
 {
-    internal class TestClass
+    public class TestCase
     {
-        HResult SomeFunction()
+        [Theory]
+        [InlineData("operation1 result", HResult.Operation1Failed)]
+        [InlineData("operation2 result", HResult.Operation2Failed)]
+        [InlineData("operation3 result", HResult.Operation3Failed)]
+        [InlineData("operation4 result", HResult.Operation4Failed)]
+        [InlineData("never", HResult.Ok)]
+        void acceptance_test(string failWhen, HResult expectedResult)
         {
-            if(Succeeded(Operation1()))
+            var sut = new SomeClass {FailWhen = failWhen};
+
+            var result = sut.SomeFunction();
+
+            result.Should().Be(expectedResult);
+        }
+    }
+
+    public class SomeClass
+    {
+        internal string FailWhen = "some condition";
+
+        internal HResult SomeFunction()
+        {
+            if (Succeeded(Operation1()))
             {
-                if(Succeeded(Operation2()))
+                if (Succeeded(Operation2()))
                 {
-                    if(Succeeded(Operation3()))
+                    if (Succeeded(Operation3()))
                     {
-                        if(Succeeded(Operation4()))
+                        if (Succeeded(Operation4()))
                         {
                             return HResult.Ok;
                         }
@@ -40,26 +63,27 @@
             // some operations
             return "operation1 result";
         }
+
         private string Operation2()
         {
             // some operations
             return "operation2 result";
         }
+
         private string Operation3()
         {
             // some operations
             return "operation3 result";
-
         }
+
         private string Operation4()
         {
             // some operations
             return "operation4 result";
-
         }
 
         private bool Succeeded(string operationResult) =>
-            operationResult == "some condition";
+            operationResult != FailWhen;
     }
 
     internal enum HResult
