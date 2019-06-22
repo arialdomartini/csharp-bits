7212bbafea29a4607acb89855ded0dc602c97651
Arialdo Martini <arialdomartini@gmail.com>
2019-06-22 14:44:45 +0200

    Generator is transformed manually

--- src/CSharpBits.Test/AnoterStateMonadTest.cs
+++ src/CSharpBits.Test/AnoterStateMonadTest.cs
@@ -19,11 +19,11 @@ namespace CSharpBits.Test
 
         class Item
         {
-            public string Product { get; set; }
-            public int Quantity { get; set; }
-            public decimal Price { get; set; }
+            internal string Product { get; set; }
+            internal int Quantity { get; set; }
+            internal decimal Price { get; set; }
 
-            public Item(int quantity, string product, decimal price)
+            internal Item(int quantity, string product, decimal price)
             {
                 Quantity = quantity;
                 Product = product;
@@ -31,23 +31,51 @@ namespace CSharpBits.Test
             }
         }
 
-        delegate (decimal, string) StatefulComputation(Item item, decimal total);
+        delegate (TResult, decimal) StatefulComputation<in TSource, TResult>(TSource item, decimal total);
 
         [Fact]
         public void stateless_cart()
         {
-            StatefulComputation cart = (item, total) => (
-                total + item.Price * item.Quantity,
-                $"Adding {item.Product}");
+            StatefulComputation<Item, string> cart = (item, total) =>
+            (
+                $"Adding {item.Product}",
+                total + item.Price * item.Quantity
+            );
 
-            var (tot1, log1) = cart(new Item(2, "zucchine", 1.0m), 0);
-            var (tot2, log2) = cart(new Item(1, "book", 20m), tot1);
-            var (tot3, log3) = cart(new Item(2, "wine", 5.0m), tot2);
+            var (log1, tot1) = cart(new Item(2, "zucchine", 1.0m), 0);
+            var (log2, tot2) = cart(new Item(1, "book", 20m), tot1);
+            var (log3, tot3) = cart(new Item(2, "wine", 5.0m), tot2);
 
             log1.Should().Be("Adding zucchine");
             log2.Should().Be("Adding book");
             log3.Should().Be("Adding wine");
             tot3.Should().Be(2 * 1.0m + 20m + 2 * 5m);
+        }
+
+        [Fact]
+        public void should_transform_generator()
+        {
+            StatefulComputation<Item, Item> cart = (item, total) =>
+            (
+                item,
+                total + item.Price * item.Quantity
+            );
+
+            StatefulComputation<Item, string> cartString = (item, total) =>
+            {
+                var (result, newTotal) = cart(item, total);
+                return ($"Adding {result.Product}", newTotal);
+            };
+
+            var (log1, tot1) = cartString(new Item(2, "zucchine", 1.0m), 0);
+            var (log2, tot2) = cartString(new Item(1, "book", 20m), tot1);
+            var (log3, tot3) = cartString(new Item(2, "wine", 5.0m), tot2);
+
+            log1.Should().Be("Adding zucchine");
+            log2.Should().Be("Adding book");
+            log3.Should().Be("Adding wine");
+            tot3.Should().Be(2 * 1.0m + 20m + 2 * 5m);
+
         }
     }
 
