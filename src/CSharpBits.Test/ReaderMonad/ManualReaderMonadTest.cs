7df5db84aa03b7645646cef241bbc2afb330aaf1
Arialdo Martini <arialdomartini@gmail.com>
2021-12-26 00:03:07 +0100

    Functor's map as an extension method of Func

--- src/CSharpBits.Test/ReaderMonad/ManualReaderMonadTest.cs
+++ src/CSharpBits.Test/ReaderMonad/ManualReaderMonadTest.cs
@@ -10,11 +10,14 @@ namespace CSharpBits.Test.ManualReaderMonad
 
     internal static class ReaderExtensions
     {
+        internal static Func<A, Func<B, C>> Curried<A, B, C>(this Func<A, B, C> f) =>
+            a => b => f(a, b);
+
         internal static A Run<E, A>(this Func<E, A> reader, E env) =>
             reader(env);
 
-        internal static Func<A, Func<B,C>> Curried<A, B, C>(this Func<A, B, C> f) =>
-            a => b => f(a, b);
+        internal static Func<E, B> Map<E, A, B>(this Func<E, A> reader, Func<A, B> f) =>
+            env => f(reader.Run(env));
     }
 
     public class ManualReaderMonadTest
@@ -22,13 +25,10 @@ namespace CSharpBits.Test.ManualReaderMonad
         string Greet(string name, Env env) =>
             $"I'm greeting {name} while Env={env}";
 
-        Func<Env, string> CurriedGreet(string name) => (Env env) =>
-            $"I'm greeting {name} while Env={env}";
-
         [Fact]
         void run_a_function()
         {
-            Func<string,int,string> func = Greet;
+            Func<string, int, string> func = Greet;
 
             var greet = func.Curried();
 
@@ -37,6 +37,20 @@ namespace CSharpBits.Test.ManualReaderMonad
             var result = applied.Run(42);
 
             result.Should().Be("I'm greeting Mario while Env=42");
+        }
+
+        [Fact]
+        void apply_a_function_to_a_function_like_functor_map()
+        {
+            Func<string, string> toLower = s => s.ToLower();
+
+            Func<string, int, string> func = Greet;
+
+            var greet = func.Curried();
+
+            var result = greet("Mario").Map(toLower).Run(42);
+
+            result.Should().Be("i'm greeting mario while env=42");
         }
     }
 }
