6999802e34ff4d5b617c2b0479ad16a45100009d
Arialdo Martini <arialdomartini@gmail.com>
2022-01-23 01:04:31 +0100

    Depenency Injection via IEnumerable

--- src/CSharpBits.Test/PuzzlingTests.cs
+++ src/CSharpBits.Test/PuzzlingTests.cs
@@ -1,12 +1,67 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using FluentAssertions;
 using Xunit;
+using static System.Environment;
 using static CSharpBits.Test.TestHelper;
 
 namespace CSharpBits.Test
 {
+    static class EnumerableDependencyInjection
+    {
+        internal static void Execute<T>(this IEnumerable<T> enumerable) =>
+            enumerable.Aggregate((_, v) => v);
+
+        internal static IEnumerable<T> DisguiseAsIEnumerable<T>(this Action a)
+        {
+            IEnumerable<T> arbitraryCode()
+            {
+                yield return default(T);
+                a();
+            }
+
+            return arbitraryCode();
+        }
+    }
+
+    public class DependencyInjectionViaIEnumerable
+    {
+        class MyTargetClass
+        {
+            private readonly IEnumerable<int> _dependency;
+
+            internal MyTargetClass(IEnumerable<int> dependency) =>
+                _dependency = dependency;
+
+            internal void SomeMethod() =>
+                _dependency.Execute();
+        }
+
+
+        [Fact]
+        void dependency_injection_with_IEnumerable()
+        {
+            var writer = new StringWriter();
+            Console.SetOut(writer);
+
+            void ToBeInjected()
+            {
+                Console.WriteLine("It worked");
+            }
+
+            IEnumerable<int> dependencyAsIEnumerable =
+                ((Action)ToBeInjected).DisguiseAsIEnumerable<int>();
+
+            new MyTargetClass(dependencyAsIEnumerable)
+                .SomeMethod();
+
+            Assert.Equal($"It worked{NewLine}", writer.GetStringBuilder().ToString());
+        }
+    }
+
+
     public class PuzzlingTest
     {
         class TargetClass
@@ -14,7 +69,7 @@ namespace CSharpBits.Test
             internal void SomeMethod(IEnumerable<int> numbers)
             {
                 // [...]
-                numbers.Select(_ => "I'm just accessing the collection of numbers").ToList();
+                numbers.ToList();
                 // [...]
             }
         }
@@ -24,11 +79,17 @@ namespace CSharpBits.Test
         [Fact]
         void function_injection_via_IEnumerable()
         {
-            IEnumerable<int> arbitraryCode()
+            void ToBeInjected()
             {
                 testResult = "Hey! arbitraryCode() was executed";
-                yield return 0;
             }
+
+            IEnumerable<int> arbitraryCode()
+            {
+                yield return 0;
+                ToBeInjected();
+            }
+
             IEnumerable<int> looksLikeNumbers = arbitraryCode();
             Assert.Equal("arbitraryCode() hasn't been run", testResult);
 
@@ -38,27 +99,27 @@ namespace CSharpBits.Test
             testResult.Should().Be("Hey! arbitraryCode() was executed");
         }
 
+
         [Fact]
         public void intermittent_test2()
         {
             IEnumerable<string> strings = SomeTexts;
-            Assert.Equal(new[]{"foo", "bar", "baz"}, strings.ToArray());
+            Assert.Equal(new[] { "foo", "bar", "baz" }, strings.ToArray());
 
             var toUpper1 = strings.Select(s => s.ToUpper());
-            Assert.Equal(new[]{"FOO", "BAR", "BAZ"}, toUpper1.ToArray());
+            Assert.Equal(new[] { "FOO", "BAR", "BAZ" }, toUpper1.ToArray());
 
 
             "".Invoking(_ =>
-            {
-                strings.Select(s => s.ToUpper() + "")
-                    .ToArray();
-
-            }).Should()
+                {
+                    strings.Select(s => s.ToUpper() + "")
+                        .ToArray();
+                }).Should()
                 .Throw<PlatformNotSupportedException>();
 
 
             var toUpper3 = strings.Select(s => "" + s.ToUpper());
-            Assert.Equal(new[]{"FOO", "BAR", "BAZ"}, toUpper3.ToArray());
+            Assert.Equal(new[] { "FOO", "BAR", "BAZ" }, toUpper3.ToArray());
         }
 
 
@@ -66,27 +127,29 @@ namespace CSharpBits.Test
         public void intermittent_test()
         {
             IEnumerable<string> strings = SomeStrings;
-            Assert.Equal(new[]{"Foo", "Bar", "Baz"}, strings.ToArray());
+            Assert.Equal(new[] { "Foo", "Bar", "Baz" }, strings.ToArray());
 
             var test1 = strings.Select(s => new string(s.ToCharArray()));
-            Assert.Equal(new[]{"Foo", "Bar", "Baz"}, test1.ToArray());
+            Assert.Equal(new[] { "Foo", "Bar", "Baz" }, test1.ToArray());
 
 
-            var test2 = strings.Select(s => new string(s.ToCharArray())+ "");
-            Assert.Equal(new[] {
+            var test2 = strings.Select(s => new string(s.ToCharArray()) + "");
+            Assert.Equal(new[]
+            {
                 "Argument Foo of type System.String is not assignable to parameter type int32",
                 "Argument Bar of type System.String is not assignable to parameter type int32",
                 "Constructor 'Baz' has 0 parameter(s) but is invoked with 0 argument(s)"
             }, test2.ToArray());
 
             var test3 = strings.Select(s => "" + new string(s.ToCharArray()));
-            Assert.Equal(new[]{"Foo", "Bar", "Baz"}, test3.ToArray());
+            Assert.Equal(new[] { "Foo", "Bar", "Baz" }, test3.ToArray());
         }
     }
 
     internal static class TestHelper
     {
         private static int count1;
+
         internal static IEnumerable<string> SomeTexts
         {
             get
@@ -106,6 +169,7 @@ namespace CSharpBits.Test
         }
 
         private static int count2;
+
         internal static IEnumerable<string> SomeStrings
         {
             get
@@ -123,11 +187,7 @@ namespace CSharpBits.Test
                     yield return "Argument Bar of type System.String is not assignable to parameter type int32";
                     yield return "Constructor 'Baz' has 0 parameter(s) but is invoked with 0 argument(s)";
                 }
-
-
             }
         }
     }
-
-
 }
