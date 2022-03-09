58ee24b78b47d0ee78a1deece969e4564cf3de4d
Arialdo Martini <arialdomartini@gmail.com>
2022-03-09 08:47:34 +0100

    Reviewed with Chris

--- src/CSharpBits.Test/Diacritics/DiacriticsConverter.cs
+++ src/CSharpBits.Test/Diacritics/DiacriticsConverter.cs
@@ -11,7 +11,6 @@ namespace CSharpBits.Test.Diacritics
             return s.Where(IsAllowed);
         }
 
-
         internal static bool IsAllowed(char c)
         {
             return Regex.IsMatch(c.ToString(), @"[a-zA-Z0-9\s\-]");
@@ -22,62 +21,63 @@ namespace CSharpBits.Test.Diacritics
             return Regex.Replace(s, @"[^a-zA-Z0-9\s\-]", "");
         }
 
-        private static string RemoveControl(this string s) =>
-            Regex.Replace(s, @"[\u0000-\u001F]", string.Empty);
+        private static string RemoveControl(this string s)
+        {
+            return Regex.Replace(s, @"[\u0000-\u001F]", string.Empty);
+        }
 
         internal static string WithConvertedSpecialCases(this string text)
         {
             var maps = new Dictionary<string, string>
             {
                 // Lower case
-                ["ä"] = "ae",
-                ["ö"] = "oe",
-                ["ü"] = "ue",
+                {"ä",  "ae"},
+                {"ö",  "oe"},
+                {"ü",  "ue"},
 
                 // Upper case, when followed by lower case
-                ["Ä(?=[a-z0-9äöüß ])"] = "Ae",
-                ["Ö(?=[a-z0-9äöüß ])"] = "Oe",
-                ["Ü(?=[a-z0-9äöüß ])"] = "Ue",
+                {"Ä(?=[a-z0-9äöüß ])", "Ae"},
+                {"Ö(?=[a-z0-9äöüß ])", "Oe"},
+                {"Ü(?=[a-z0-9äöüß ])", "Ue"},
 
                 // Upper case, when followed by upper case
-                ["Ä(?=[A-ZÄÖÜ])"] = "AE",
-                ["Ö(?=[A-ZÄÖÜ])"] = "OE",
-                ["Ü(?=[A-ZÄÖÜ])"] = "UE",
+                {"Ä(?=[A-ZÄÖÜ])", "AE"},
+                {"Ö(?=[A-ZÄÖÜ])", "OE"},
+                {"Ü(?=[A-ZÄÖÜ])", "UE"},
 
                 // Upper case, single letter
-                ["Ä"] = "Ae",
-                ["Ö"] = "Oe",
-                ["Ü"] = "Ue",
+                {"Ä",  "Ae"},
+                {"Ö",  "Oe"},
+                {"Ü",  "Ue"},
 
 
-                ["ß"] = "ss",
+                {"ß",  "ss"},
 
-                ["_"] = "-",
-                ["ç"] = "c",
-                ["Ç"] = "C",
-                ["Ë"] = "E",
-                ["ë"] = "e",
-                ["ñ"] = "n",
-                ["Ñ"] = "N",
-                ["Õ"] = "O",
-                ["õ"] = "o",
-                ["à"] = "a",
-                ["è"] = "e",
-                ["é"] = "e",
-                ["Ï"] = "I",
-                ["ï"] = "i",
-
-                ["ì"] = "i",
-                ["ò"] = "o",
-                ["ù"] = "u",
-                ["À"] = "A",
-                ["Ê"] = "E",
-                ["ê"] = "e",
-                ["È"] = "E",
-                ["É"] = "E",
-                ["Ì"] = "I",
-                ["Ò"] = "O",
-                ["Ù"] = "U"
+                {"_", "-"},
+                {"ç", "c"},
+                {"Ç", "C"},
+                {"Ë", "E"},
+                {"ë", "e"},
+                {"ñ", "n"},
+                {"Ñ", "N"},
+                {"Õ", "O"},
+                {"õ", "o"},
+                {"à", "a"},
+                {"è", "e"},
+                {"é", "e"},
+                {"Ï", "I"},
+                {"ï", "i"},
+                {"ì", "i"},
+                {"ò", "o"},
+                {"ù", "u"},
+                {"À", "A"},
+                {"Ê", "E"},
+                {"ê", "e"},
+                {"È", "E"},
+                {"É", "E"},
+                {"Ì", "I"},
+                {"Ò", "O"},
+                {"Ù", "U"}
             };
 
             return
