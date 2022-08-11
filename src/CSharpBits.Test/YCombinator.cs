febfbb4f11cd00e97e312b408f57702205efb0ec
Arialdo Martini <arialdomartini@gmail.com>
2022-08-11 14:31:33 +0200

    Step 2: inject continuation

--- src/CSharpBits.Test/YCombinator.cs
+++ src/CSharpBits.Test/YCombinator.cs
@@ -1,5 +1,7 @@
-﻿using FsCheck;
+﻿using FluentAssertions;
+using FsCheck;
 using FsCheck.Xunit;
+using Xunit;
 using static FsCheck.Prop;
 
 namespace CSharpBits.Test
@@ -9,11 +11,19 @@ namespace CSharpBits.Test
         private readonly Arbitrary<int> PositiveNumbers = Arb.From(Gen.Choose(0, 15_000));
 
         private delegate int Sum(int n);
+        private delegate Sum PartSum(Sum continuation);
 
-        private static readonly Sum sum =
-            n =>
-                n == 0 ? 0 : n + sum(n - 1);
-
+        private static readonly PartSum mkSum =
+            continuation =>
+                n =>
+                    n == 0 ? 0 : n + continuation(n - 1);
+        
+        private static readonly Sum ad_hoc_continuation = n => n * (n + 1) / 2;
+        
+        // Note: order does matter! To make it order-independent, use
+        // private static readonly Sum sum = n => mkSum(ad_hoc_continuation)(n);
+        private static readonly Sum sum = mkSum(ad_hoc_continuation);
+        
         [Property]
         Property it_meets_the_gauss_formula() =>
             ForAll(PositiveNumbers, n =>
