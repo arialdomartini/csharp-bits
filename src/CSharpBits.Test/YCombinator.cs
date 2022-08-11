68e83ab3886a95f37571058e2e2a8da5e5d7e767
Arialdo Martini <arialdomartini@gmail.com>
2022-08-11 14:12:52 +0200

    Func<int, int> aliased with type Sum

--- src/CSharpBits.Test/YCombinator.cs
+++ src/CSharpBits.Test/YCombinator.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using FsCheck;
+﻿using FsCheck;
 using FsCheck.Xunit;
 using static FsCheck.Prop;
 
@@ -8,11 +7,12 @@ namespace CSharpBits.Test
     public class YCombinator
     {
         private readonly Arbitrary<int> PositiveNumbers = Arb.From(Gen.Choose(0, 15_000));
-     
-        private static readonly Func<int, int> sum =
-            n => n == 0 ?
-                0 :
-                n + sum(n - 1);
+
+        private delegate int Sum(int n);
+
+        private static readonly Sum sum =
+            n =>
+                n == 0 ? 0 : n + sum(n - 1);
 
         [Property]
         Property it_meets_the_gauss_formula() =>
