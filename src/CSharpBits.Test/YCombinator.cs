e89e65bf2acdd55ff04511155a5d677f7b586af1
Arialdo Martini <arialdomartini@gmail.com>
2022-08-11 13:57:22 +0200

    Step 1: A recursive sum(), covered with a Property Test

---
+++ src/CSharpBits.Test/YCombinator.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using FsCheck;
+using FsCheck.Xunit;
+using static FsCheck.Prop;
+
+namespace CSharpBits.Test
+{
+    public class YCombinator
+    {
+        private readonly Arbitrary<int> PositiveNumbers = Arb.From(Gen.Choose(0, 15_000));
+     
+        private static readonly Func<int, int> sum =
+            n => n == 0 ?
+                0 :
+                n + sum(n - 1);
+
+        [Property]
+        Property it_meets_the_gauss_formula() =>
+            ForAll(PositiveNumbers, n =>
+                sum(n) == n * (n + 1) / 2);
+    }
+}
