318e2675db4c71a07ea4895158bfe7ac7327e1f7
Arialdo Martini <arialdomartini@gmail.com>
2022-08-12 09:07:42 +0200

    Step 1: a recursive sum function

--- src/CSharpBits.Test/YCombinator.cs
+++ src/CSharpBits.Test/YCombinator.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using FsCheck;
+﻿using FsCheck;
 using FsCheck.Xunit;
 using static FsCheck.Prop;
 
@@ -7,33 +6,13 @@ namespace CSharpBits.Test
 {
     public class YCombinator
     {
-        // Notice! We had to reduce the max recursive depth. Why?
         private readonly Arbitrary<int> PositiveNumbers = Arb.From(Gen.Choose(0, 8_000));
 
         private delegate int Sum(int n);
-        private delegate Sum PartSum(Sum continuation);
 
-        private static readonly PartSum mkSum =
-            continuation =>
+        private static readonly Sum sum =
                 n =>
-                    n == 0 ? 0 : n + continuation(n - 1);
-        
-        // This is replaced by Y(f)
-        // private static readonly Sum ad_hoc_continuation = n => n * (n + 1) / 2;
-
-
-        // A function that given a PartSum returns a Sum.
-        // By definition, Y(f) is a correct sum. So we can replace ad_hoc_implementation with it, and
-        // feed it to PartSum
-        
-        // Replacing the eager evaluation with a lambda makes this works with C# too
-        private static readonly Func<PartSum, Sum> Y = 
-            f =>
-                n =>
-                    f(Y(f))(n);
-
-        // private static readonly Sum sum = mkSum(ad_hoc_continuation);
-        private static readonly Sum sum = Y(mkSum);
+                    n == 0 ? 0 : n + sum(n - 1);
         
         [Property]
         Property it_meets_the_gauss_formula() =>
