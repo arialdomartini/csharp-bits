138ead990617313fbf051b9b4e38a1962715f00b
Arialdo Martini <arialdomartini@kpmg.com>
2024-05-15 07:59:16 +0200

    State monad as explained by The book of Monads

---
+++ src/CSharpBits.Test/StateMonad/StateMonadTest.cs
@@ -0,0 +1,40 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using Xunit;
+using static CSharpBits.Test.StateMonad.Tree;
+#pragma warning disable CS8509 // The switch expression does not handle all possible values of its input type (it is not exhaustive).
+
+namespace CSharpBits.Test.StateMonad;
+
+public class StateMonadTes2
+{
+    [Fact]
+    void count_the_leaves()
+    {
+        Tree tree =
+            new Node(
+                new Node(
+                    new Leaf("one"),
+                    new Leaf("two")),
+                new Leaf("three"));
+       
+        var numberOfLeaves = CountLeaves(tree);
+
+        Assert.Equal(3, numberOfLeaves);
+    }
+
+    private static int CountLeaves(Tree tree) =>
+        tree switch
+        {
+            Leaf => 1,
+            Node { Left: var left, Right: var right } => CountLeaves(left) + CountLeaves(right)
+//          Node node => countLeaves(node.Left) + countLeaves(node.Right)
+        };
+}
+
+[SuppressMessage("ReSharper", "InconsistentNaming")]
+internal interface Tree
+{
+    internal record Leaf(string Value) : Tree;
+
+    internal record Node(Tree Left, Tree Right) : Tree;
+}
