5dfebc04f6ba3a8652c7c6a0fda5c8b07521ce27
Arialdo Martini <arialdomartini@kpmg.com>
2024-05-15 19:26:32 +0200

    Lighter tests

--- src/CSharpBits.Test/StateMonad/StateMonadTest.cs
+++ src/CSharpBits.Test/StateMonad/StateMonadTest.cs
@@ -11,15 +11,23 @@ namespace CSharpBits.Test.StateMonad;
 [SuppressMessage("ReSharper", "InconsistentNaming")]
 public class StateMonadTes2
 {
+    private Tree<string>.Node TreeWith3Leaves = new Tree<string>.Node(
+        new Tree<string>.Node(
+            new Tree<string>.Leaf("one"),
+            new Tree<string>.Leaf("two")),
+        new Tree<string>.Leaf("three"));
+
+    private static Tree<(int, string)>.Node LabeledTree = new Tree<(int, string)>.Node(
+        new Tree<(int, string)>.Node(
+            new Tree<(int, string)>.Leaf((1, "one")),
+            new Tree<(int, string)>.Leaf((2, "two"))),
+        new Tree<(int, string)>.Leaf((3, "three")));
+
     [Fact]
     void count_the_leaves()
     {
         Tree<string> tree =
-            new Tree<string>.Node(
-                new Tree<string>.Node(
-                    new Tree<string>.Leaf("one"),
-                    new Tree<string>.Leaf("two")),
-                new Tree<string>.Leaf("three"));
+            TreeWith3Leaves;
 
         var numberOfLeaves = CountLeaves(tree);
 
@@ -37,23 +45,11 @@ public class StateMonadTes2
     [Fact]
     void relabel_tree()
     {
-        Tree<string> tree =
-            new Tree<string>.Node(
-                new Tree<string>.Node(
-                    new Tree<string>.Leaf("one"),
-                    new Tree<string>.Leaf("two")),
-                new Tree<string>.Leaf("three"));
+        Tree<string> tree = TreeWith3Leaves;
 
         var (labelled, count) = relabel(tree)(1);
 
-        Tree<(int, string)> expected =
-            new Tree<(int, string)>.Node(
-                new Tree<(int, string)>.Node(
-                    new Tree<(int, string)>.Leaf((1, "one")),
-                    new Tree<(int, string)>.Leaf((2, "two"))),
-                new Tree<(int, string)>.Leaf((3, "three")));
-
-        Assert.Equal(expected, labelled);
+        Assert.Equal(LabeledTree, labelled);
         Assert.Equal(4, count);
     }
     
@@ -82,22 +78,4 @@ internal interface Tree<t>
     internal record Leaf(t Value) : Tree<t>;
 
     internal record Node(Tree<t> Left, Tree<t> Right) : Tree<t>;
-}
-
-internal record WithCounter<a>(Func<int, (a, int)> Apply);
-
-[SuppressMessage("ReSharper", "UnusedMember.Global")]
-[SuppressMessage("ReSharper", "InconsistentNaming")]
-internal static class WithCounterExtensions
-{
-    internal static
-        WithCounter<b> next<a, b>(this WithCounter<a> f, Func<a, WithCounter<b>> g)
-    {
-        return new WithCounter<b>(counter0 =>
-        {
-            var (r1, counter1) = f.Apply(counter0);
-            var (r2, counter2) = g(r1).Apply(counter1);
-            return (r2, counter2);
-        });
-    }
 }
