f7808b930c40c81e838d0a68033decfbba32e9dd
Arialdo Martini <arialdomartini@kpmg.com>
2024-05-15 16:31:03 +0200

    Relabel tree, with manual state management

--- src/CSharpBits.Test/StateMonad/StateMonadTest.cs
+++ src/CSharpBits.Test/StateMonad/StateMonadTest.cs
@@ -1,40 +1,83 @@
 ﻿using System.Diagnostics.CodeAnalysis;
+using FluentAssertions.Equivalency;
 using Xunit;
-using static CSharpBits.Test.StateMonad.Tree;
+
+#pragma warning disable CS8981 // The type name only contains lower-cased ascii characters. Such names may become reserved for the language.
 #pragma warning disable CS8509 // The switch expression does not handle all possible values of its input type (it is not exhaustive).
 
 namespace CSharpBits.Test.StateMonad;
 
+[SuppressMessage("ReSharper", "InconsistentNaming")]
 public class StateMonadTes2
 {
     [Fact]
     void count_the_leaves()
     {
-        Tree tree =
-            new Node(
-                new Node(
-                    new Leaf("one"),
-                    new Leaf("two")),
-                new Leaf("three"));
-       
+        Tree<string> tree =
+            new Tree<string>.Node(
+                new Tree<string>.Node(
+                    new Tree<string>.Leaf("one"),
+                    new Tree<string>.Leaf("two")),
+                new Tree<string>.Leaf("three"));
+
         var numberOfLeaves = CountLeaves(tree);
 
         Assert.Equal(3, numberOfLeaves);
     }
 
-    private static int CountLeaves(Tree tree) =>
+    private static int CountLeaves<t>(Tree<t> tree) =>
         tree switch
         {
-            Leaf => 1,
-            Node { Left: var left, Right: var right } => CountLeaves(left) + CountLeaves(right)
+            Tree<t>.Leaf => 1,
+            Tree<t>.Node { Left: var left, Right: var right } => CountLeaves(left) + CountLeaves(right)
 //          Node node => countLeaves(node.Left) + countLeaves(node.Right)
         };
+
+    [Fact]
+    void relabel_tree()
+    {
+        Tree<string> tree =
+            new Tree<string>.Node(
+                new Tree<string>.Node(
+                    new Tree<string>.Leaf("one"),
+                    new Tree<string>.Leaf("two")),
+                new Tree<string>.Leaf("three"));
+
+        var (labelled, count) = relabel(tree, 1);
+
+        Tree<(int, string)> expected =
+            new Tree<(int, string)>.Node(
+                new Tree<(int, string)>.Node(
+                    new Tree<(int, string)>.Leaf((1, "one")),
+                    new Tree<(int, string)>.Leaf((2, "two"))),
+                new Tree<(int, string)>.Leaf((3, "three")));
+
+        Assert.Equal(expected, labelled);
+        Assert.Equal(4, count);
+    }
+
+    (Tree<(int, string)>, int) relabel(Tree<string> tree, int counter)
+    {
+        return tree switch
+        {
+            Tree<string>.Leaf l => (new Tree<(int, string)>.Leaf((counter, l.Value)), counter + 1),
+            Tree<string>.Node node => goInside(node, counter)
+        };
+
+        (Tree<(int, string)>, int) goInside(Tree<string>.Node node, int i0)
+        {
+            var (relabeledLeft, i1) = relabel(node.Left, i0);
+            var (relabeledRght, i2) = relabel(node.Right, i1);
+            return
+                (new Tree<(int, string)>.Node(Left: relabeledLeft, Right: relabeledRght), i2);
+        }
+    }
 }
 
 [SuppressMessage("ReSharper", "InconsistentNaming")]
-internal interface Tree
+internal interface Tree<t>
 {
-    internal record Leaf(string Value) : Tree;
+    internal record Leaf(t Value) : Tree<t>;
 
-    internal record Node(Tree Left, Tree Right) : Tree;
+    internal record Node(Tree<t> Left, Tree<t> Right) : Tree<t>;
 }
