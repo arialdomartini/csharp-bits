9eb7b64900ab87d2a276bc7950e7fb52310bfb68
Arialdo Martini <arialdo.martini@gmail.com>
2025-07-18 11:47:22 +0200

    Applicative, with fallback

--- src/CSharpBits.Test/ApplicativeStyle/ApplicativeStyle.cs
+++ src/CSharpBits.Test/ApplicativeStyle/ApplicativeStyle.cs
@@ -12,16 +12,12 @@ record Manager(string Name);
 record Person(string Name, int Age, Manager Manager)
 {
     internal static Func<string, Func<int, Func<Manager, Person>>> Build => name => age => manager => new(name, age, manager);
+
 }
 
 abstract record Result<V>
 {
     internal abstract V GetValue();
-}
-
-internal record Failure<V>(List<string> Errors) : Result<V>
-{
-    internal override V GetValue() => throw new ApplicationException("Not a Success");
 }
 
 internal record Success<V>(
@@ -30,46 +26,64 @@ internal record Success<V>(
     internal override V GetValue() => Value;
 }
 
+internal record Failure<V>(V DefaultValue, List<string> Errors) : Result<V>
+{
+    internal override V GetValue() => DefaultValue;
+}
+
 internal static class ResultInstances
 {
+    internal static Failure<V> FailWith<V>(V value, string errorMessage) => new(value, [errorMessage]);
+    internal static Failure<V> FailWith<V>(V value, List<string> errorMessages) => new(value, errorMessages);
+    internal static Success<V> SucceedWith<V>(V value) => new(value);
+
+    private static List<string> Merge(List<string> a, List<string> b) => a.Append(b).ToList();
+
     internal static Result<A> Point<A>(A a) => new Success<A>(a);
 
-    internal static Success<Func<A, Func<B, Func<C, D>>>> Apply<A,B,C,D>(this Func<A,B,C,D> f) =>
-        new(a => b => c => f(a, b, c));
+
+    private static Result<B> Map<A,B>(Func<A, B> f, Result<A> a) =>
+        a switch
+        {
+            Success<A> success => SucceedWith(f(success.Value)),
+            Failure<A> failure => FailWith(f(failure.DefaultValue), failure.Errors)
+        };
 
     internal static Result<B> Ap<A, B>(Result<Func<A, B>> fr, Result<A> ar) =>
         fr switch
         {
-            Failure<Func<A, B>> error =>
+            Failure<Func<A, B>> failedF =>
                 ar switch
                 {
-                    Failure<A> failure => new Failure<B>(error.Errors.Append(failure.Errors).ToList()),
-                    Success<A> => new Failure<B>(error.Errors),
+                    Failure<A> failedA => FailWith(failedF.DefaultValue(failedA.DefaultValue), Merge(failedF.Errors, failedA.Errors)),
+                    Success<A> a => FailWith(failedF.DefaultValue(a.Value), failedF.Errors),
                 },
 
 
             Success<Func<A, B>> f =>
                 ar switch
                 {
-                    Failure<A> error => new Failure<B>(error.Errors),
+                    Failure<A> failedA => FailWith(f.Value(failedA.DefaultValue), failedA.Errors),
                     Success<A> a => new Success<B>(f.Value(a.Value))
                 }
         };
 
+    internal static Success<Func<A, Func<B, Func<C, D>>>> Apply<A,B,C,D>(this Func<A,B,C,D> f) =>
+        new(a => b => c => f(a, b, c));
+
     internal static Result<B> With<A, B>(this Result<Func<A, B>> fr, Result<A> ar) => Ap(fr, ar);
 
-    private static Result<B> Map<A,B>(Func<A, B> f, Result<A> a) =>
-        a switch
+    private static Result<B> Bind<A, B>(Func<A, Result<B>> f, Result<A> ar) =>
+        ar switch
         {
-            Success<A> success => new Success<B>(f(success.Value)),
-            Failure<A> failure => new Failure<B>(failure.Errors)
-        };
+            Success<A> a => f(a.Value),
+            Failure<A> failedA =>
+                f(failedA.DefaultValue) switch
+                {
+                    Failure<B> failedFApplication => FailWith(failedFApplication.DefaultValue, Merge(failedFApplication.Errors, failedA.Errors)),
+                    Success<B> result => SucceedWith(result.Value)
+                }
 
-    private static Result<B> Bind<A, B>(Func<A, Result<B>> f, Result<A> a) =>
-        a switch
-        {
-            Success<A> success => f(success.Value),
-            Failure<A> failure => new Failure<B>(failure.Errors)
         };
 
     private static Result<B> Select<A, B>(this Result<A> a, Func<A, B> f) =>
@@ -93,6 +107,7 @@ internal static class ResultInstances
 public class ApplicativeStyle
 {
     private Manager Maria = new Manager(Name: "Maria");
+    private Manager Richa = new Manager(Name: "Richa");
 
     [Fact]
     void successfully_build_a_person()
@@ -114,9 +129,9 @@ public class ApplicativeStyle
     [Fact]
     void error_case()
     {
-        Result<string> GetName() => new Success<string>("Shansai");
-        Result<int> GetAge() => new Failure<int>(["too old!"]);
-        Result<Manager> GetManager() => new Failure<Manager>(["no one wants to work with him!"]);
+        Result<string> GetName() => SucceedWith("Shansai");
+        Result<int> GetAge() => FailWith(42, "too old!");
+        Result<Manager> GetManager() => FailWith(Richa, "no one wants to work with him!");
 
         var result =
             Ap(Ap(Ap(Point(
@@ -130,15 +145,16 @@ public class ApplicativeStyle
         List<string> expectedErrors = ["too old!", "no one wants to work with him!"];
 
         Assert.Equivalent(expectedErrors, ((Failure<Person>)result).Errors);
+        Assert.Equal(new Person("Shansai", 42, Richa), result.GetValue());
     }
 
     [Fact]
     void error_case_better_syntax()
     {
         // example of API calls. Some of them would fail
-        Result<string> GetName() => new Success<string>("Shansai");
-        Result<int> GetAge() => new Failure<int>(["too old!"]);
-        Result<Manager> GetManager() => new Failure<Manager>(["no one wants to work with him!"]);
+        Result<string> GetName() => SucceedWith("Shansai");
+        Result<int> GetAge() => FailWith(42, "too old!");
+        Result<Manager> GetManager() => FailWith(Richa, "no one wants to work with him!");
 
         // factory for a person
         Func<string, int, Manager, Person> buildPerson = (name, age, manager) => new Person(name, age, manager);
@@ -157,20 +173,22 @@ public class ApplicativeStyle
         List<string> expectedErrors = ["too old!", "no one wants to work with him!"];
 
         Assert.Equivalent(expectedErrors, ((Failure<Person>)result).Errors);
+        Assert.Equal(new Person("Shansai", 42, Richa), result.GetValue());
     }
 
     [Fact]
     void error_case_with_LINQ_would_capture_first_error_only()
     {
-        Result<string> GetName() => new Success<string>("Shansai");
-        Result<int> GetAge() => new Failure<int>(["too old!"]);
-        Result<Manager> GetManager() => new Failure<Manager>(["no one wants to work with him!"]);
+        Result<string> GetName() => SucceedWith("Shansai");
+        Result<int> GetAge() => FailWith(42, "too old!");
+        Result<Manager> GetManager() => FailWith(Richa, "no one wants to work with him!");
 
 
         var result =
             from name in GetName()
             from age in GetAge()
             from manager in GetManager()
+
             select new Person(name, age, manager);
 
         Assert.IsType<Failure<Person>>(result);
