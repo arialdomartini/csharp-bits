ce887fbcd761db9c7437c73837aba5b006851377
Arialdo Martini <arialdo.martini@gmail.com>
2025-08-08 09:27:27 +0200

    LeetCode, sum number

---
+++ src/CSharpBits.Test/LeetCode/NumberSum.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Xunit;
+using static CSharpBits.Test.LeetCode.Quiz2AddTwoNumbers;
+
+namespace CSharpBits.Test.LeetCode;
+
+public class Quiz2AddTwoNumbersTest
+{
+    [Fact]
+    internal void Sum_of_3_items_lists()
+    {
+        List<int> one = [2, 4, 3];
+        List<int> two = [5, 6, 4];
+        List<int> exp = [7, 0, 8];
+
+        var res = Sum(one, two);
+
+        Assert.Equivalent(exp, res);
+    }
+
+    [Fact]
+    internal void Sum_of_3_items_lists_with_last_remainder()
+    {
+        List<int> one = [2, 4, 5];
+        List<int> two = [5, 6, 4];
+        List<int> exp = [7, 0, 0, 1];
+
+        var res = Sum(one, two);
+
+        Assert.Equivalent(exp, res);
+    }
+
+    [Theory]
+    [InlineData(1, 1, 0)]
+    [InlineData(9, 9, 0)]
+    [InlineData(10, 0, 10)]
+    [InlineData(11, 1, 10)]
+    [InlineData(18, 8, 10)]
+    void Test_floor10(int n, int floored, int remainder)
+    {
+        var (f, r) = n.Floor10();
+
+        Assert.Equal(floored, f);
+        Assert.Equal(r, remainder);
+    }
+}
+
+file static class Quiz2AddTwoNumbers
+{
+    internal static (int, int) Floor10(this int n)
+    {
+        var m = n % 10;
+        var remainder = n - m;
+        return (m, remainder);
+    }
+
+    private static (int, List<int>) Calc((int Remainder, List<int> Result) acc, (int Left, int Right) el)
+    {
+        var sum = el.Left + el.Right + acc.Remainder;
+        var (s, r) = sum.Floor10();
+
+        return (r / 10, acc.Result.Append([s]).ToList());
+    }
+
+    private static (int, List<int>) Sum(this IEnumerable<(int, int)> zs) =>
+        zs.Aggregate((0, new List<int>()), Calc);
+
+    internal static List<int> Sum(List<int> l1, List<int> l2)
+    {
+        var (r, res) = l1.Zip(l2).Sum();
+        return r > 0
+            ? res.Append([r]).ToList()
+            : res;
+    }
+}
---
+++ src/CSharpBits.Test/LeetCode/NumberSumListNodeCycle.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Xunit;
+using static CSharpBits.Test.LeetCode.Helpers;
+
+namespace CSharpBits.Test.LeetCode;
+
+public class Quiz2AddTwoNumbersTestListNodeCycle
+{
+    [Fact]
+    void stringify()
+    {
+        List<int> exp = [7, 0, 9];
+        var s = exp.ToListNode().Stringify();
+
+        Assert.Equal("709", s);
+    }
+
+    [Fact]
+    internal void Sum_of_3_items_lists_with_ListNode()
+    {
+        List<int> one = [2, 4, 5];
+        List<int> two = [5, 6, 4];
+        List<int> exp = [7, 0, 0, 1];
+
+        var res = Sum(one.ToListNode(), two.ToListNode());
+
+        Assert.Equal("7001", res.Stringify());
+    }
+
+    [Fact]
+    internal void Sum_of_3_4_items_lists_with_ListNode()
+    {
+        List<int> one = [2, 4, 5, 3];
+        List<int> two = [5, 6, 4];
+        List<int> exp = [7, 0, 0, 4];
+
+        var res = Sum(one.ToListNode(), two.ToListNode());
+
+        Assert.Equal("7004", res.Stringify());
+    }
+
+    [Theory]
+    [InlineData(1, 1, 0)]
+    [InlineData(9, 9, 0)]
+    [InlineData(10, 0, 10)]
+    [InlineData(11, 1, 10)]
+    [InlineData(18, 8, 10)]
+    void floor10(int n, int floored, int remainder)
+    {
+        var (f, r) = n.Floor10();
+
+        Assert.Equal(floored, f);
+        Assert.Equal(r, remainder);
+    }
+}
+
+file static class Helpers
+{
+    internal static string Stringify(this ListNode listNode)
+    {
+        if (listNode.IsEmpty()) return "";
+        else return $"{listNode.Val}{listNode.Next.Stringify()}";
+    }
+
+    internal static ListNode ToListNode(this List<int> l) =>
+        l.Count == 0
+            ? null
+            : new ListNode(l[0], l[1..].ToListNode());
+
+    internal static (int, int) Floor10(this int n)
+    {
+        var m = n % 10;
+        var remainder = n - m;
+        return (m, remainder);
+    }
+
+    private static bool IsEmpty(this ListNode l) => l == null;
+
+    private static int NVal(this ListNode l) => l.IsEmpty() ? 0 : l.Val;
+    private static ListNode NNext(this ListNode l) => l.IsEmpty() ? null : l.Next;
+
+    private static ListNode SumAcc(ListNode l1, ListNode l2)
+    {
+        ListNode current = null;
+        int remainder = 0;
+        while (!l1.IsEmpty() || !l2.IsEmpty())
+        {
+            var sum = l1.NVal() + l2.NVal() + remainder;
+            var (res, r) = sum.Floor10();
+            remainder = r / 10;
+
+            current = new ListNode(res, current);
+            l1 = l1.NNext();
+            l2 = l2.NNext();
+        }
+
+        return remainder > 0
+            ? new ListNode(remainder, current)
+            : current;
+    }
+
+    private static ListNode Reverse(this ListNode head)
+    {
+        ListNode reversed = null;
+        var current = head;
+        while (current != null)
+        {
+            reversed = new ListNode(current.Val, reversed);
+            current = current.Next;
+        }
+
+        return reversed;
+    }
+
+    internal static ListNode Sum(ListNode l1, ListNode l2) => SumAcc(l1, l2).Reverse();
+}
+
+// Provided by LeetCode
+file class ListNode
+{
+    internal readonly int Val;
+    internal readonly ListNode Next;
+
+    internal ListNode(int val = 0, ListNode next = null)
+    {
+        Val = val;
+        Next = next;
+    }
+}
