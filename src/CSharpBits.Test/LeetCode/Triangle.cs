687d3b76d1a7436d29d8f5877e7195b5df0d77d6
Arialdo Martini <arialdo.martini@gmail.com>
2025-08-13 09:36:33 +0200

    Leet code Triangle

---
+++ src/CSharpBits.Test/LeetCode/Triangle.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Xunit;
+
+namespace CSharpBits.Test.LeetCode;
+
+public class Triangle
+{
+    int[][] Sums(int[][] triangle)
+    {
+        int[][] PathsFrom(int level)
+        {
+            if (level == triangle.Length - 1)
+                return triangle[level].Select(p => (int[])[p]).ToArray();
+
+            var next = PathsFrom(level + 1);
+
+            return
+                triangle[level]
+                    .Select((thisElement, thisI) => next[thisI].Append(next[thisI+1])
+                        .Select(n => n + thisElement).ToArray()).ToArray();
+        }
+
+        return PathsFrom(0);
+    }
+
+
+
+    [Fact]
+    void find_all_path_sums()
+    {
+        int[][] input = [[2],[3,4],[6,5,7],[4,1,8,3]];
+
+        int minumumSum = Sums(input)[0].Min();
+
+        Assert.Equal(11, minumumSum);
+    }
+}
